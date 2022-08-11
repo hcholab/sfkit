8d46b392bdeb4b89c59152b97962dba3f16a7c54
Simon Mendelsohn <simonjmendelsohn@gmail.com>
2022-08-11 11:32:10 -0400

    install plink and minor changes

--- src/sfkit/protocol/utils/sfgwas_protocol.py
+++ src/sfkit/protocol/utils/sfgwas_protocol.py
@@ -2,6 +2,7 @@ import fileinput
 import os
 
 import toml
+from google.cloud import firestore
 from sfkit.protocol.utils import constants
 from sfkit.protocol.utils.helper_functions import run_command
 
@@ -20,10 +21,12 @@ def run_sfgwas_protocol(doc_ref_dict: dict, role: str) -> None:
 def install_sfgwas() -> None:
     print("Begin installing dependencies")
     commands = """sudo apt-get update -y
-                    sudo apt-get install python3-pip wget git -y
+                    sudo apt-get install python3-pip wget git zip unzip -y
                     sudo wget https://golang.org/dl/go1.18.1.linux-amd64.tar.gz
                     sudo tar -C /usr/local -xzf go1.18.1.linux-amd64.tar.gz
-                    sudo echo 'export PATH=$PATH:/usr/local/go/bin' >> .bashrc 
+                    sudo wget https://s3.amazonaws.com/plink2-assets/alpha3/plink2_linux_avx2_20220603.zip
+                    sudo unzip -o plink2_linux_avx2_20220603.zip -d /usr/local
+                    sudo echo 'export PATH=$PATH:/usr/local/' >> .bashrc 
                     source .bashrc # cannot use sudo because source is a shell command, not an independent program
                     sudo pip3 install numpy"""
     for command in commands.split("\n"):
@@ -35,17 +38,24 @@ def install_sfgwas() -> None:
         print("Installing lattigo")
         run_command("git clone https://github.com/hcholab/lattigo && cd lattigo && git switch lattigo_pca")
 
+    # need to get token since the repos are currently private
+    doc_ref = firestore.Client().collection("meta").document("token")
+    doc_ref_dict: dict = doc_ref.get().to_dict() or {}
+    [(username, token)] = doc_ref_dict.items()
+
     if os.path.isdir("mpc-core"):
         print("mpc-core already exists")
     else:
         print("Installing mpc-core")
-        run_command("git clone https://github.com/hhcho/mpc-core")
+        run_command(f"git clone https://{username}:{token}@github.com/hhcho/mpc-core")
 
     if os.path.isdir("sfgwas-private"):
         print("sfgwas-private already exists")
     else:
         print("Installing sfgwas-private")
-        run_command("git clone https://github.com/hhcho/sfgwas-private && cd sfgwas-private && git switch release")
+        run_command(
+            f"git clone https://{username}:{token}@github.com/hhcho/sfgwas-private && cd sfgwas-private && git switch release"
+        )
 
     print("Finished installing dependencies")
 
@@ -62,7 +72,7 @@ def update_config_files(doc_ref_dict: dict, role: str, configuration: str) -> No
             update_data_path_in_config_file_lungPgen(role, data_path)
         else:
             raise ValueError(f"unknown configuration: {configuration}")
-    update_ip_addresses_and_ports_in_config_fille(doc_ref_dict, configuration)
+    update_configGlobal(doc_ref_dict, configuration)
 
 
 def update_data_path_in_config_file_lungGCPFinal(role: str, data_path: str) -> None:
@@ -92,13 +102,19 @@ def update_data_path_in_config_file_lungPgen(role: str, data_path: str) -> None:
         toml.dump(data, f)
 
 
-def update_ip_addresses_and_ports_in_config_fille(doc_ref_dict: dict, configuration: str) -> None:
+def update_configGlobal(doc_ref_dict: dict, configuration: str) -> None:
     config_file_path = f"sfgwas-private/config/{configuration}/configGlobal.toml"
     data = toml.load(config_file_path)
 
+    # TODO: remove these when the config files are already upadated?
     data["use_precomputed_geno_count"] = True
     data["num_inds"] = [0, 4585, 4513]
 
+    print("Checking NUM_INDS")
+    for i, participant in enumerate(doc_ref_dict["participants"]):
+        assert doc_ref_dict["personal_parameters"][participant]["NUM_INDS"]["value"] == str(data["num_inds"][i])
+
+    # Update the ip addresses and ports
     for i, participant in enumerate(doc_ref_dict["participants"]):
         ip_addr = doc_ref_dict["personal_parameters"][participant]["IP_ADDRESS"]["value"]
         data["servers"][f"party{i}"]["ipaddr"] = ip_addr
@@ -119,6 +135,8 @@ def build_sfgwas(configuration: str) -> None:
     for line in fileinput.input(files="sfgwas-private/main_test.go", inplace=True):
         if "var defaultConfigPath = " in line:
             print(f'var defaultConfigPath = "config/{configuration}"')
+        elif "func TestGwas(t *testing.T) {" in line:
+            print("func TestGwasSimonDemo(t *testing.T) {")
         else:
             print(line, end="")
 
@@ -137,7 +155,7 @@ def update_batch_run(role: str) -> None:
         elif "END=" in line:
             print(f"END={role}")
         elif "TESTNAME=" in line:
-            print("TESTNAME=TestGwas")
+            print("TESTNAME=TestGwasSimonDemo")
         else:
             print(line, end="")
 
