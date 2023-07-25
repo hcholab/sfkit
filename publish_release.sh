bcce4cb098bf556f01470d740370ae5b48687401
Denis Loginov <git@dinvlad.dev>
2023-07-25 19:47:06 -0400

    Fix wait for asset packaging

--- publish_release.sh
+++ publish_release.sh
@@ -31,35 +31,12 @@ for p in ${platforms} ; do
         rm -rf "${tmp_dir}"
     ) &
 done
+wait
 
 last_release=$(gh release list -L 1 | awk '{print $3}')
 next_release=$(perl -pe 's/(\d+)$/($1+1)/e' <<< "${last_release}")
 
-gh release create --generate-notes --notes-start-tag "${last_release}" "${next_release}"
-
-# Currently, we have to retry upload of each asset,
-# due to intermittent issues with file uploads:
-# https://github.com/cli/cli/issues/7750
-for f in "${archive_base}"* ; do
-    (
-        n=1
-        max=3
-        while true ; do
-            if gh release upload "${next_release}" "$f" ; then
-                break
-            else
-                if [[ $n -lt $max ]]; then
-                    sleep $n
-                    ((n++))
-                    echo "Retrying upload for $f (attempt $n/$max):"
-                else
-                    echo "Upload for $f failed after $n attempts."
-                    exit 1
-                fi
-            fi
-        done
-    ) &
-done
-wait -n
+gh release create --generate-notes \
+    --notes-start-tag "${last_release}" "${next_release}" "${archive_base}"*
 
 rm -rf "${dist_dir}"
