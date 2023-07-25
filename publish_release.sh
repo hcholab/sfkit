4e73872a31588de6c21d30ef79da3e894c085b2a
Denis Loginov <git@dinvlad.dev>
2023-07-25 19:13:58 -0400

    Re-arrange publish script to upload each asset right after packaging it

--- publish_release.sh
+++ publish_release.sh
@@ -3,21 +3,16 @@
 # Extracts platform-specific TAR archives
 # containing static executables from the OCI image,
 # and publishes them in a GitHub release.
-#
-# For more details, see
-# https://stackoverflow.com/a/72952846/2962507
 
 set -euxo pipefail
 
 IMAGE=$1
 
-dist_dir="$(pwd)/dist"
-rm -rf "${dist_dir}"
-mkdir -p "${dist_dir}"
+last_release=$(gh release list -L 1 | awk '{print $3}')
+next_release=$(perl -pe 's/(\d+)$/($1+1)/e' <<< "${last_release}")
+gh release create --generate-notes --notes-start-tag "${last_release}" "${next_release}"
 
-archive_base="${dist_dir}/sfkit_"
 sfkit_dir="sfkit/"
-
 platforms=$(
     docker buildx imagetools inspect "${IMAGE}" \
         --format '{{range .Manifest.Manifests}}{{with .Platform}}{{if (ne .OS "unknown")}}  {{.OS}}/{{.Architecture}}{{if .Variant}}/{{.Variant}}{{end}}{{end}}{{end}}{{end}}'
@@ -27,43 +22,35 @@ for p in ${platforms} ; do
         tmp_dir=$(mktemp -d)
         pushd "${tmp_dir}"
 
+        # extract sfkit dir from the platform image
         crane export "${IMAGE}" - --platform "$p" | tar -xf - "${sfkit_dir}"
-        tar -czf "${archive_base}${p//\//_}.tar.gz" "${sfkit_dir}"
+
+        # compress that dir into archive asset
+        asset="${p//\//_}.tar.gz"
+        tar -czf "${asset}" "${sfkit_dir}"
+
+        # Currently, we have to retry upload of each asset,
+        # due to intermittent issues with file uploads:
+        # https://github.com/cli/cli/issues/7750
+        n=1
+        max=3
+        while true ; do
+            if gh release upload "${next_release}" "${asset}" ; then
+                break
+            else
+                if [[ $n -lt $max ]]; then
+                    sleep $n
+                    ((n++))
+                    echo "Retrying upload for ${asset} (attempt $n/$max):"
+                else
+                    echo "Upload for ${asset} failed after $n attempts."
+                    exit 1
+                fi
+            fi
+        done
 
         popd
         rm -rf "${tmp_dir}"
     ) &
 done
 wait -n
-
-last_release=$(gh release list -L 1 | awk '{print $3}')
-next_release=$(perl -pe 's/(\d+)$/($1+1)/e' <<< "${last_release}")
-
-gh release create --generate-notes --notes-start-tag "${last_release}" "${next_release}"
-
-# Currently, we have to retry upload of each asset,
-# due to intermittent issues with file uploads:
-# https://github.com/cli/cli/issues/7750
-for f in "${archive_base}"* ; do
-    (
-        n=1
-        max=3
-        while true ; do
-            if gh release upload "${next_release}" "$f" ; then
-                break
-            else
-                if [[ $n -lt $max ]]; then
-                    sleep $n
-                    ((n++))
-                    echo "Retrying upload for $f (attempt $n/$max):"
-                else
-                    echo "Upload for $f failed after $n attempts."
-                    exit 1
-                fi
-            fi
-        done
-    ) &
-done
-wait -n
-
-rm -rf "${dist_dir}"
