758071622d3697e5eb1b72cc8de071816fe31687
Simon Mendelsohn <simonjmendelsohn@gmail.com>
2024-01-18 20:04:03 -0500

    fix bugs

--- sfkit/api.py
+++ sfkit/api.py
@@ -1,34 +1,47 @@
 from io import IOBase
 
+import google.auth
 import requests
+from google.auth.transport.requests import Request as GAuthRequest
 
 from sfkit.utils import constants
 
+AUTH_HEADER = "Authorization"
+BEARER_PREFIX = "Bearer "
 
 def website_send_file(file: IOBase, filename: str) -> bool:
+    params = {}
     files = {"file": (filename, file)}
     url = f"{constants.SFKIT_API_URL}/upload_file"
     with open(constants.AUTH_KEY, "r") as f:
         auth_key = f.readline().rstrip()
 
-    headers = {
-        "Authorization": f"{auth_key}",
-        # "content-type": "application/json",
-    }
-    response = requests.post(url, files=files, headers=headers)
+    if auth_key.startswith("study_id:"):
+        params["study_id"] = auth_key.split(":")[1]
+        headers = get_service_account_headers()
+    else:
+        headers = {
+            "Authorization": f"{auth_key}",
+            "content-type": "application/json",
+        }
+
+    response = requests.post(url, files=files, headers=headers, params=params)
 
     return response.status_code == 200
 
 
-def website_get(request_type: str, params: dict = None, headers: dict = None) -> requests.Response:
+def website_get(request_type: str, params: dict = None) -> requests.Response:
     if params is None:
         params = {}
     url = f"{constants.SFKIT_API_URL}/{request_type}"
 
-    if headers is None:
-        with open(constants.AUTH_KEY, "r") as f:
-            auth_key = f.readline().rstrip()
-
+    with open(constants.AUTH_KEY, "r") as f:
+        auth_key = f.readline().rstrip()
+    
+    if auth_key.startswith("study_id:"):
+        params["study_id"] = auth_key.split(":")[1]
+        headers = get_service_account_headers()
+    else:
         headers = {
             "Authorization": f"{auth_key}",
             "content-type": "application/json",
@@ -41,8 +54,8 @@ def get_doc_ref_dict() -> dict:
     response = website_get("get_doc_ref_dict")
     return response.json()
 
-def get_study_options(headers: dict) -> dict:
-    response = website_get("get_study_options", headers=headers)
+def get_study_options() -> dict:
+    response = website_get("get_study_options")
     return response.json()
 
 def get_username() -> str:
@@ -59,3 +72,13 @@ def update_firestore(msg: str) -> bool:
 def create_cp0() -> bool:
     response = website_get("create_cp0")
     return response.status_code == 200
+
+
+def get_service_account_headers():
+    print("Trying to authenticate with Google Cloud Platform...")
+    creds, _ = google.auth.default()
+    creds = creds.with_scopes(["openid", "email", "profile"])
+    creds.refresh(GAuthRequest())
+    return {
+        AUTH_HEADER: BEARER_PREFIX + creds.token,
+    }
