5effdf620f038b6ff931b94787318e15ea2c704a
Denis Loginov <137337+dinvlad@users.noreply.github.com>
2024-04-02 17:39:01 -0400

    Auto-create SOCK_PATH dir (#33)
    * Auto-create SOCK_PATH dir
    * Adjust chmod for socket dir and file
    * Bump sfkit-proxy to 91b2a07 (dependabot)

--- sfkit/sidecar/utils.py
+++ sfkit/sidecar/utils.py
@@ -6,7 +6,5 @@ from sfkit.utils import constants
 def get_sock_path():
     sock_path = constants.SOCK_PATH
     sock_dir = os.path.dirname(sock_path)
-    if not os.path.exists(sock_dir):
-        print(f"\nDirectory {sock_dir} does not exist. Please create it or set SFKIT_SOCK to a valid path\n")
-        exit(1)
+    os.makedirs(sock_dir, mode=0o770, exist_ok=True)
     return sock_path
