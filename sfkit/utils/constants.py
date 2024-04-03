cfd03fb48ae54a4121301344694413ab357ca96c
Denis Loginov <137337+dinvlad@users.noreply.github.com>
2024-04-03 16:33:03 -0400

    Skip printing SFKIT_API_URL for client and help (#34)
    * Skip printing SFKIT_API_URL for client and help
    
    * Fix command exemptions

--- sfkit/utils/constants.py
+++ sfkit/utils/constants.py
@@ -7,7 +7,6 @@ def is_installed(binary: str) -> bool:
 
 
 SFKIT_API_URL = os.environ.get("SFKIT_API_URL", "https://sfkit-website-bhj5a4wkqa-uc.a.run.app/api")
-print(f"SFKIT_API_URL: {SFKIT_API_URL}")
 METADATA_VM_IDENTITY_URL = (
     "http://metadata.google.internal/computeMetadata/v1/"
     "instance/service-accounts/default/identity?"
