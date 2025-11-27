85dd59969e2a248fca51f073232acc18c939810d
Denis Loginov <denis@stratologics.com>
2025-11-26 22:36:32 -0500

    Restore apt commands in install.sh

--- install.sh
+++ install.sh
@@ -51,10 +51,9 @@ echo
 
 echo Installing sfkit...
 cd sfkit
-#if apt -v ; then
-#  apt update
-#  apt install -y --no-install-recommends python3-venv
-#fi
+if apt -v ; then
+  sudo apt update && sudo apt install -y --no-install-recommends python3-venv || true
+fi
 python3 -m venv .venv
 source .venv/bin/activate
 pip install --no-user patchelf ./sfkit*.whl
