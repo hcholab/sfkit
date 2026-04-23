cd25b07671b327996736b2dcb8a462ef69d32a51
Denis Loginov <denis@stratologics.com>
2026-04-22 21:53:06 -0400

    Use versioned URL in release notes

--- publish_release.sh
+++ publish_release.sh
@@ -36,7 +36,7 @@ done
 wait
 
 gh release create --generate-notes \
-    -n "To install **sfkit**, please run:<br/>\`source <(curl -sL https://github.com/hcholab/sfkit/releases/latest/download/install.sh)\`<br/>" \
+    -n "To install **sfkit**, please run:<br/>\`source <(curl -sL https://github.com/hcholab/sfkit/releases/download/${VERSION}/install.sh)\`<br/>" \
     "${VERSION}" "install.sh" "${archive_base}"*
 
 rm -rf "${dist_dir}"
